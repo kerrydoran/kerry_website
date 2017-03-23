600117a35ce7b06d4fca6835ac36af8d936d8156
kerrydoran <kerry.doran@gmail.com>
2017-03-23 00:00:51 -0400

    added show list in shows module and edited curatorial view

--- shows.js
+++ shows.js
@@ -1,6 +1,72 @@
 module.exports = {
+	reas: {
+		title: 'Casey Reas: There\'s No Distance' ,
+		gallery: 'bitforms gallery, New York',
+		link: 'http://www.bitforms.com/exhibitions/reas-2016',
+		press: [{
+			text: 'Artspace',
+			url: 'http://www.artspace.com/magazine/interviews_features/artist_to_watch/artists-to-watch-in-september-54150'
+		}, {
+			text: 'The Creators Project',
+			url: 'https://creators.vice.com/en_us/article/casey-reas-new-media-art-bitforms-15-years'
+		}, {
+			text: 'Village Voice',
+			url: 'http://www.villagevoice.com/arts/casey-reas-s-disconcerting-software-paintings-9215303'
+		}]
+	},
+	kanarek: {
+		title: 'Kisses Kisses',
+		gallery: 'bitforms gallery, New York',
+		link: 'http://www.bitforms.com/exhibitions/kanarek-2016',
+		press: [{
+			text: 'Artspace',
+			url: 'http://www.artspace.com/magazine/news_events/artist_to_watch/artists-to-watch-this-april-53658'
+		}, {
+			text: 'The Huffington Post',
+			url: 'http://www.huffingtonpost.com/illya-szilak/art-review-yael-kanareks_b_9778852.html'
+		}, {
+			text: 'New York Times',
+			url: 'https://www.nytimes.com/2016/04/10/arts/design/yael-kanarek-has-her-fifth-show-at-bitforms.html'
+		}]
+	},
+	ludy: {
+		title: 'Sara Ludy: Subsurface Hell',
+		gallery: 'bitforms gallery, New York'
+		link: 'http://www.bitforms.com/exhibitions/ludy-2016',
+		press: [{
+			text: 'ArtFCity',
+			url: 'http://artfcity.com/2016/01/04/this-weeks-must-see-events-gallery-armageddon/'
+		}, {
+			text: 'Artforum',
+			url: 'https://www.artforum.com/index.php?pn=picks&id=57561&view=print'
+		}, {
+			text: 'The Creators Project',
+			url: 'https://creators.vice.com/en_us/article/sara-ludy-subsurface-hell'
+		}, {
+			text: 'Hyperallergic',
+			url: 'http://hyperallergic.com/272392/turning-the-digital-into-the-material-with-feng-shui/'
+		}, {
+			text: 'Modern Painters',
+			url: ''
+		}, {
+			text: 'NewHive',
+			url: 'https://newhive.com/b/this-week-in-net-art-11/'
+		}]
+	},
+	bianchic: {
+		title: 'Emilio Bianchic: Nailture',
+		gallery: 'Postmasters Gallery, New York',
+		link: 'http://www.postmastersart.com/archive/nailture15/nailture15.html',
+		press: [{
+			text: '',
+			url: ''
+		}, {
+			text: '',
+			url: ''
+		}]
+	},
 	showName: {
-		title: 'fdsafsad',
+		title: 'text here',
 		gallery: 'text here',
 		link: '',
 		press: [{
@@ -22,6 +88,4 @@ module.exports = {
 			text: '',
 			url: ''
 		}]
-	},
-
 };
